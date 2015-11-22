e4e1042f3042e466d8e475e969c0654da05ce97f
Scott Bellware <sbellware@gmail.com>
2015-11-22 16:41:24 -0600

    The copy arg can be used instead of include

---
+++ test/spec/copy_is_alias_for_include.rb
@@ -0,0 +1,36 @@
+require_relative 'spec_init'
+
+module Fixture
+  module CopyAlias
+    class Example
+      attr_accessor :some_attribute
+      attr_accessor :some_other_attribute
+      attr_accessor :yet_another_attribute
+    end
+
+    def self.data
+      {
+        some_attribute: 'some value',
+        some_other_attribute: 'some other value',
+        yet_another_attribute: 'yet another value'
+      }
+    end
+
+    def self.example
+      Example.new
+    end
+  end
+end
+
+describe "Copied Attributes" do
+  it "Are set" do
+    receiver = Fixture::CopyAlias.example
+    data = Fixture::CopyAlias.data
+
+    SetAttributes.(receiver, data, copy: [:some_other_attribute, :yet_another_attribute])
+
+    assert(receiver.some_attribute.nil?)
+    assert(receiver.some_other_attribute == 'some other value')
+    assert(receiver.yet_another_attribute == 'yet another value')
+  end
+end
