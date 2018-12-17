41ea733c49f43c859b048788bb82a6de855ccb45
Scott Bellware <sbellware@gmail.com>
2018-12-17 10:15:49 -0600

    Test coverage for singular form of include

---
+++ test/automated/included_attributes/one.rb
@@ -0,0 +1,23 @@
+require_relative '../../test_init'
+
+context "Included Attributes" do
+  context "One" do
+    receiver = Controls::Object::New.example
+    data = Controls::Hash.example
+
+    SetAttributes.(receiver, data, include: :some_other_attribute)
+
+    context "Included" do
+      test "Are set" do
+        assert(receiver.some_other_attribute == 'some other value')
+      end
+    end
+
+    context "Excluded" do
+      test "Aren't set" do
+        assert(receiver.some_attribute.nil?)
+        assert(receiver.yet_another_attribute.nil?)
+      end
+    end
+  end
+end
