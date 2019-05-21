0aee00cca44c3015449544127041c46173de1ca7
Scott Bellware <sbellware@gmail.com>
2019-05-21 11:59:39 -0400

    Source, rather than data

--- lib/set_attributes/data_source/hash.rb
+++ lib/set_attributes/data_source/hash.rb
@@ -1,29 +1,29 @@
 class SetAttributes
   module DataSource
     class Hash
-      attr_reader :data
+      attr_reader :source
       attr_reader :attribute_map
 
-      def initialize(data, attribute_map)
-        @data = data
+      def initialize(source, attribute_map)
+        @source = source
         @attribute_map = attribute_map
       end
 
-      def self.build(data, include=nil, exclude: nil)
+      def self.build(source, include=nil, exclude: nil)
         if include.nil? || include.empty?
-          include = data.keys
+          include = source.keys
         end
 
         attribute_map = SetAttributes::Map.build(include)
 
-        instance = new(data, attribute_map)
+        instance = new(source, attribute_map)
         instance.exclude(exclude)
         instance
       end
 
       def get_value(attribute)
         source_attribute = attribute_map[attribute]
-        data[source_attribute]
+        source[source_attribute]
       end
 
       def exclude(*attributes)
--- lib/set_attributes/data_source/object.rb
+++ lib/set_attributes/data_source/object.rb
@@ -3,29 +3,29 @@ class SetAttributes
     class Object
       Error = Class.new(RuntimeError)
 
-      attr_reader :data
+      attr_reader :source
       attr_reader :attribute_map
 
-      def initialize(data, attribute_map)
-        @data = data
+      def initialize(source, attribute_map)
+        @source = source
         @attribute_map = attribute_map
       end
 
-      def self.build(data, include=nil, exclude: nil)
+      def self.build(source, include=nil, exclude: nil)
         if include.nil?
-          raise Error, "Object data source is missing the include mapping"
+          raise Error, "Object source source is missing the include mapping"
         end
 
         attribute_map = SetAttributes::Map.build(include)
 
-        instance = new(data, attribute_map)
+        instance = new(source, attribute_map)
         instance.exclude(exclude)
         instance
       end
 
       def get_value(attribute)
         source_attribute = attribute_map[attribute]
-        data[source_attribute]
+        source[source_attribute]
       end
 
       def exclude(*attributes)
