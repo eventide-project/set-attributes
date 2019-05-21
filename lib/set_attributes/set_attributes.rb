c868d73888e0e3b0dc7ae9d002db599c723d9d5a
Scott Bellware <sbellware@gmail.com>
2019-05-20 23:54:40 -0400

    Obsolete code is removed

--- lib/set_attributes/set_attributes.rb
+++ lib/set_attributes/set_attributes.rb
@@ -1,16 +1,6 @@
 class SetAttributes
   attr_reader :data_source
 
-  # def include
-  #   @include ||= []
-  # end
-  # attr_writer :include
-
-  # def exclude
-  #   @exclude ||= []
-  # end
-  # attr_writer :exclude
-
   def strict
     @strict ||= false
   end
@@ -23,20 +13,6 @@ class SetAttributes
   def self.build(receiver, data, copy: nil, include: nil, exclude: nil, strict: nil)
     strict ||= false
 
-    ## Future: Data Source
-
-      ## Remove this when objects are supported
-      ## Refer this responsibility to object data source
-      # unless data.respond_to? :to_h
-      #   raise ArgumentError, "#{data} can't be used to set attributes. It can't be converted to Hash."
-      # end
-
-      # unless data.is_a? Hash
-      #   data = data.to_h
-      # end
-
-    ##
-
     exclude ||= []
     exclude = Array(exclude)
 
@@ -47,16 +23,9 @@ class SetAttributes
     include ||= []
     include = Array(include)
 
-    ## This is too late. It's only hash data source that knows this.
-    ## Hash data source has to construct the attribute map?
-    # include = data.keys if include.empty?
-
     data_source = DataSource::Hash.build(data, include, exclude: exclude)
 
-    # new(receiver, data).tap do |instance|
     new(data_source).tap do |instance|
-      # instance.include = include
-      # instance.exclude = exclude
       instance.strict = strict
     end
   end
@@ -67,28 +36,7 @@ class SetAttributes
   end
 
   def call(receiver, data)
-    ## ---
-    ## Shoring. Remove. Scott Mon May 20 2019
-    # unless data.is_a? Hash
-    #   data = data.to_h
-    # end
-    ## -
-
-    ## Legacy
-    ## include_mapping = self.include_mapping
-    ## attributes = (data.keys & include_mapping.keys) - exclude
-
     set_attributes = []
-    ## attributes.each do |from_attribute|
-    ##   to_attribute = include_mapping[from_attribute]
-
-    ##   value = data[from_attribute]
-
-    ##   Set.(receiver, to_attribute, value, strict: strict)
-
-    ##   set_attributes << to_attribute
-    ## end
-
     data_source.attribute_map.each_mapping do |source_attribute, receiver_attribute|
       value = data_source.get_value(source_attribute)
 
