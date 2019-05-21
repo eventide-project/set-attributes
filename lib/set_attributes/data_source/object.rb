abb33c9835f9ad3c99c69b6e29b94248e5bd6e46
Scott Bellware <sbellware@gmail.com>
2019-05-21 13:59:59 -0400

    Object data source is a specialized DataSource

--- lib/set_attributes/data_source/object.rb
+++ lib/set_attributes/data_source/object.rb
@@ -1,35 +1,21 @@
 class SetAttributes
   module DataSource
     class Object
+      include DataSource
+
       Error = Class.new(RuntimeError)
 
-      attr_reader :source
-      attr_reader :attribute_map
-
-      def initialize(source, attribute_map)
-        @source = source
-        @attribute_map = attribute_map
-      end
-
-      def self.build(source, include=nil, exclude: nil)
+      def self.assure_include(source, include)
         if include.nil?
           raise Error, "Object source source is missing the include mapping"
         end
 
-        attribute_map = SetAttributes::Map.build(include)
-
-        instance = new(source, attribute_map)
-        instance.exclude(exclude)
-        instance
+        return include
       end
 
       def get_value(attribute)
         source_attribute = attribute_map[attribute]
-        source[source_attribute]
-      end
-
-      def exclude(*attributes)
-        attribute_map.exclude(attributes)
+        source.send(source_attribute)
       end
     end
   end
