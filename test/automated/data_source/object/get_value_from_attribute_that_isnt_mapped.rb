b1aa517cf11c3d74f05d549a3a5ef30443003886
Scott Bellware <sbellware@gmail.com>
2019-05-21 14:58:33 -0400

    Test file names are standardized

---
+++ test/automated/data_source/object/get_value_from_attribute_that_isnt_mapped.rb
@@ -0,0 +1,19 @@
+require_relative '../../../test_init'
+
+context "Data Source" do
+  context "Hash" do
+    context "Get Value from Source Attribute that Isn't Mapped" do
+      mapping = Controls::AttributeMap::Unbalanced.data
+
+      data = SetAttributes::Controls::Hash.example
+
+      hash_source = SetAttributes::DataSource::Hash.build(data, mapping)
+
+      value = hash_source.get_value(SecureRandom.hex.to_sym)
+
+      test "Source value is retrieved by the receiver attribute name" do
+        assert(value.nil?)
+      end
+    end
+  end
+end
