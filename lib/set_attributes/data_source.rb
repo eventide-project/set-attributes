d28829f59c4c47ac6bd5fea28b5b0944caf0d326
Scott Bellware <sbellware@gmail.com>
2019-05-21 11:54:43 -0400

    Factory selects and constructs class

--- lib/set_attributes/data_source.rb
+++ lib/set_attributes/data_source.rb
@@ -1,11 +1,15 @@
 class SetAttributes
   module DataSource
     def self.build_data_source(source, include=nil, exclude: nil)
-      ## Class selector, rather than direct construction
+      data_source_class = select_class(source)
+      data_source_class.build(source, include, exclude: exclude)
+    end
+
+    def self.select_class(source)
       if source.is_a?(::Hash)
-        return DataSource::Hash.build(source, include, exclude: exclude)
-      elsif source.is_a?(::Object)
-        return DataSource::Object.build(source, include, exclude: exclude)
+        return DataSource::Hash
+      else
+        return DataSource::Object
       end
     end
   end
