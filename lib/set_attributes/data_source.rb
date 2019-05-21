1ec8d349d6375845902359610739c944cdbd3761
Scott Bellware <sbellware@gmail.com>
2019-05-21 13:56:33 -0400

    Data source generalities are in the DataSource module

--- lib/set_attributes/data_source.rb
+++ lib/set_attributes/data_source.rb
@@ -1,5 +1,35 @@
 class SetAttributes
   module DataSource
+    def self.included(cls)
+      cls.class_exec do
+        extend Build
+      end
+    end
+
+    attr_reader :source
+    attr_reader :attribute_map
+
+    def initialize(source, attribute_map)
+      @source = source
+      @attribute_map = attribute_map
+    end
+
+    def exclude(*attributes)
+      attribute_map.exclude(attributes)
+    end
+
+    module Build
+      def build(source, include=nil, exclude: nil)
+        include = assure_include(source, include)
+
+        attribute_map = SetAttributes::Map.build(include)
+
+        instance = new(source, attribute_map)
+        instance.exclude(exclude)
+        instance
+      end
+    end
+
     def self.build_data_source(source, include=nil, exclude: nil)
       data_source_class = select_class(source)
       data_source_class.build(source, include, exclude: exclude)
--- lib/set_attributes/data_source/hash.rb
+++ lib/set_attributes/data_source/hash.rb
@@ -1,33 +1,19 @@
 class SetAttributes
   module DataSource
     class Hash
-      attr_reader :source
-      attr_reader :attribute_map
+      include DataSource
 
-      def initialize(source, attribute_map)
-        @source = source
-        @attribute_map = attribute_map
-      end
-
-      def self.build(source, include=nil, exclude: nil)
+      def self.assure_include(source, include)
         if include.nil? || include.empty?
-          include = source.keys
+          return source.keys
         end
 
-        attribute_map = SetAttributes::Map.build(include)
-
-        instance = new(source, attribute_map)
-        instance.exclude(exclude)
-        instance
+        return include
       end
 
       def get_value(attribute)
         source_attribute = attribute_map[attribute]
         source[source_attribute]
-      end
-
-      def exclude(*attributes)
-        attribute_map.exclude(attributes)
       end
     end
   end
