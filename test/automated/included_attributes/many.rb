c9c5d3deb69082869161f8c9786828eea3236b60
Scott Bellware <sbellware@gmail.com>
2019-05-22 13:17:11 -0400

    Tests are parameterized

--- test/automated/included_attributes/many.rb
+++ test/automated/included_attributes/many.rb
@@ -2,24 +2,36 @@ require_relative '../../test_init'
 
 context "Included Attributes" do
   context "Many" do
-    receiver = Controls::Object::New.example
-    source = Controls::Hash.example
+    hash_source = Controls::Hash.example
+    object_source = Controls::Object.example
 
-    SetAttributes.(receiver, source, include: [
-      :some_other_attribute,
-      :yet_another_attribute
-    ])
+    mapping = hash_source.keys
 
-    context "Included" do
-      test "Are set" do
-        assert(receiver.some_other_attribute == 'some other value')
-        assert(receiver.yet_another_attribute == 'yet another value')
-      end
-    end
+    [[hash_source, 'Hash'], [object_source, 'Object']].each do |source_info|
 
-    context "Excluded" do
-      test "Aren't set" do
-        assert(receiver.some_attribute.nil?)
+      source = source_info[0]
+      source_type = source_info[1]
+
+      context "#{source_type} Source" do
+        receiver = Controls::Object::New.example
+
+        SetAttributes.(receiver, source, include: [
+          :some_other_attribute,
+          :yet_another_attribute
+        ])
+
+        context "Included" do
+          test "Are set" do
+            assert(receiver.some_other_attribute == 'some other value')
+            assert(receiver.yet_another_attribute == 'yet another value')
+          end
+        end
+
+        context "Excluded" do
+          test "Aren't set" do
+            assert(receiver.some_attribute.nil?)
+          end
+        end
       end
     end
   end
--- test/automated/included_attributes/one.rb
+++ test/automated/included_attributes/one.rb
@@ -2,21 +2,33 @@ require_relative '../../test_init'
 
 context "Included Attributes" do
   context "One" do
-    receiver = Controls::Object::New.example
-    source = Controls::Hash.example
+    hash_source = Controls::Hash.example
+    object_source = Controls::Object.example
 
-    SetAttributes.(receiver, source, include: :some_other_attribute)
+    mapping = hash_source.keys
 
-    context "Included" do
-      test "Are set" do
-        assert(receiver.some_other_attribute == 'some other value')
-      end
-    end
+    [[hash_source, 'Hash'], [object_source, 'Object']].each do |source_info|
 
-    context "Excluded" do
-      test "Aren't set" do
-        assert(receiver.some_attribute.nil?)
-        assert(receiver.yet_another_attribute.nil?)
+      source = source_info[0]
+      source_type = source_info[1]
+
+      context "#{source_type} Source" do
+        receiver = Controls::Object::New.example
+
+        SetAttributes.(receiver, source, include: :some_other_attribute)
+
+        context "Included" do
+          test "Are set" do
+            assert(receiver.some_other_attribute == 'some other value')
+          end
+        end
+
+        context "Excluded" do
+          test "Aren't set" do
+            assert(receiver.some_attribute.nil?)
+            assert(receiver.yet_another_attribute.nil?)
+          end
+        end
       end
     end
   end
