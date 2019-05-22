82921649513991602e0cee82a5b3133e51be5b40
Scott Bellware <sbellware@gmail.com>
2019-05-22 15:24:23 -0400

    Missing receiver attribute test is parameterized

--- test/automated/missing_receiver_attributes/ignored.rb
+++ test/automated/missing_receiver_attributes/ignored.rb
@@ -1,12 +1,30 @@
 require_relative '../../test_init'
 
 context "Missing Receiver Attributes" do
-  receiver = Controls::Object::MissingAttribute.example
-  source = Controls::Hash.example
+  hash_source = Controls::Hash.example
+  object_source = Controls::Object.example
 
-  test "Not an error" do
-    refute proc { SetAttributes.(receiver, source) } do
-      raises_error?
+  mapping = hash_source.keys
+
+  [[hash_source, 'Hash'], [object_source, 'Object']].each do |source_info|
+
+    source = source_info[0]
+    source_type = source_info[1]
+
+    context "#{source_type} Source" do
+      data_source, _ = SetAttributes::DataSource.build_data_source(source, mapping)
+
+      refute(data_source.get_value(:some_attribute).nil?)
+      refute(data_source.get_value(:some_other_attribute).nil?)
+      refute(data_source.get_value(:yet_another_attribute).nil?)
+
+      receiver = Controls::Object::MissingAttribute.example
+
+      test "Not an error" do
+        refute proc { SetAttributes.(receiver, source, include: mapping) } do
+          raises_error? SetAttributes::Assign::Error
+        end
+      end
     end
   end
 end
