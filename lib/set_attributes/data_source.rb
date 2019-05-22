d9232ee35cb135ab53b6959c67481f7ada17baf1
Scott Bellware <sbellware@gmail.com>
2019-05-22 15:48:25 -0400

    Implementation rather than select class

--- lib/set_attributes/data_source.rb
+++ lib/set_attributes/data_source.rb
@@ -37,11 +37,11 @@ class SetAttributes
     end
 
     def self.build_data_source(source, include=nil, exclude: nil)
-      data_source_class = select_class(source)
+      data_source_class = implementation(source)
       data_source_class.build(source, include, exclude: exclude)
     end
 
-    def self.select_class(source)
+    def self.implementation(source)
       if source.is_a?(::Hash)
         return DataSource::Hash
       else
