cdd664faf22ac6ec573d9f4bd9b54b910aa7127b
Scott Bellware <sbellware@gmail.com>
2019-05-22 14:21:10 -0400

    Parameterized test for missing source attribute

---
+++ test/automated/set_attributes/missing_source_attribute.rb
@@ -0,0 +1,41 @@
+require_relative '../../test_init'
+
+context "Set Attributes" do
+  context "Missing Source Attribute" do
+    hash_source = Controls::Hash::MissingAttribute.example
+    object_source = Controls::Object::MissingAttribute.example
+
+    mapping = hash_source.keys
+
+    [[hash_source, 'Hash'], [object_source, 'Object']].each do |source_info|
+
+      source = source_info[0]
+      source_type = source_info[1]
+
+      data_source, _ = SetAttributes::DataSource.build_data_source(source, mapping)
+
+      refute(data_source.get_value(:some_attribute).nil?)
+      refute(data_source.get_value(:some_other_attribute).nil?)
+      assert(data_source.get_value(:yet_another_attribute).nil?)
+
+      context "#{source_type} Source" do
+        receiver = Controls::Object::New.example
+
+        assert(receiver.some_attribute.nil?)
+        assert(receiver.some_other_attribute.nil?)
+        assert(receiver.yet_another_attribute.nil?)
+
+        SetAttributes.(receiver, source, include: mapping)
+
+        test "Sets attributes that correspond to the mapped source attribute" do
+          assert(receiver.some_attribute == 'some value')
+          assert(receiver.some_other_attribute == 'some other value')
+        end
+
+        test "Does not set attributes that are not mapped" do
+          assert(receiver.yet_another_attribute.nil?)
+        end
+      end
+    end
+  end
+end
