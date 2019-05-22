e1c11c60cf59db01cb1a2b3579b1f14e25b69d5c
Scott Bellware <sbellware@gmail.com>
2019-05-22 02:10:42 -0400

    Test for exclude alias for delete

---
+++ test/automated/attribute_map/exclude_alias.rb
@@ -0,0 +1,15 @@
+require_relative '../../test_init'
+
+context "Attribute Map" do
+  context "Exclude Alias for Delete" do
+    mapping = Controls::Mapping.example
+
+    attribute_map = SetAttributes::Map.build(mapping)
+
+    attribute_map.delete(:some_attribute)
+
+    test "Mapping is removed from the attribute map)" do
+      assert(attribute_map.mapping(:some_attribute).nil?)
+    end
+  end
+end
